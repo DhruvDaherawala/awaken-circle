f34b740caa81392fd25721af9bdbd997dbaf690f
Dhruv Daherawala <dhruvdaherawala1533@gmail.com>
2026-06-06 11:40:56 +0530

    chore: migrate Prisma to modern Rust-free generator with local workspace output and resolve Vercel timeouts

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../lib/generated/client/client';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import * as mariadb from 'mariadb';
 import 'dotenv/config';
--- scripts/checkAdmin.js
+++ scripts/checkAdmin.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('../lib/generated/client/client');
 const { PrismaMariaDb } = require('@prisma/adapter-mariadb');
 require('dotenv').config();
 
